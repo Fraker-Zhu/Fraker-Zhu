0303fe7d7998cd048ebcc34c1529737813b169c1
Fraker-Zhu <58595963+Fraker-Zhu@users.noreply.github.com>
2022-03-29 20:45:54 +0800

    拆分js文件

--- js/index.js
+++ js/index.js
@@ -14,16 +14,6 @@ function init() {
 
 }
 
-function SetCode(){
-    //alert($('#str').val());
-    //alert($.base64.encode($('#str').val()));
-    $('#val').val($.base64.encode($('#str').val()));
-}
-
-function SetVal(){
-    alert($.base64.decode($('#str').val()));
-}
-
 function getXhr() {
     /****
     var httpreq = null;
